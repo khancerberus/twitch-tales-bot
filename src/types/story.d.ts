79e4c8f2e1161e649651ff4de10178eaf8abbbf4
Victor Valenzuela <khancerberus@hotmail.cl>
2025-07-13 05:35:10 -0400

    chore: Define StoryGenerationProps interface for sentiment and context handling

--- src/types/story.d.ts
+++ src/types/story.d.ts
@@ -2,3 +2,8 @@ import { StoryContext } from '../config/consts'
 
 export type StorySentimentType = 'positive' | 'negative' | 'neutral';
 export type StoryContextType = typeof StoryContext[keyof typeof StoryContext];
+
+export interface StoryGenerationProps {
+    sentiment: StorySentimentType;
+    context?: StoryContextType;
+}
